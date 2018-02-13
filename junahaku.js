759671881204f3a632286b19c1d5de2fff00cae0
anvais <antti.j.valtonen@alumni.aalto.fi>
2018-02-13 13:16:40 +0200

    NikiAntin muutokset
    1. Asemalistauksen haku
    2. Dropdown menut
    3. Valittava eri lähtö ja pääteasemat

--- junahaku.js
+++ junahaku.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     req = new XMLHttpRequest();
 
     req.onreadystatechange = function () {
@@ -7,6 +8,7 @@ window.onload = function () {
                 var data = JSON.parse(req.responseText);
                 console.dir(data);
                 var ulos = document.getElementById("lista");
+                ulos.innerHTML = '';
                 for(var i = 0; i < data.length; i++){
                     var juna = data[i];
                     var lahtoaika = new Date(juna.timeTableRows[0].scheduledTime);
@@ -18,21 +20,62 @@ window.onload = function () {
                     ulos.innerHTML += "<li>" + juna.trainCategory +
                         ": " + juna.trainType + juna.trainNumber + ", lähtee (" + lahtopaikka + "): "
                         + lahtoaika.toLocaleDateString("fi", optiot) + " saapuu (" + saapumispaikka + "): "
-                        + saapumisaika.toLocaleDateString("fi", optiot) ;
+                        + saapumisaika.toLocaleDateString("fi", optiot);
                 }
             } else {
                 alert("Pyyntö epäonnistui");
             }
         }
     };
-
-
-    //testikommentti @author Tom
-    //eka kommentti
-
+    // luodaan valittujen asemien väliset yhteydet listaksi
     document.getElementById("nappi").onclick = function(){
-        req.open('GET', 'https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/SK', true);
+        var lahtoasema = document.getElementById("lahto").value;
+        var paateasema = document.getElementById("paate").value;
+        console.dir(lahtoasema);
+        console.dir(paateasema);
+        req.open('GET', 'https://rata.digitraffic.fi/api/v1/live-trains/station/' + lahtoasema + '/' + paateasema, true);
         req.send(null);
     }
-
-}
+    // luodaan lähtöasemien dynaaminen lista JSON:in avulla
+    let dropdownlahto = $('#lahto');
+    dropdownlahto.empty();
+    dropdownlahto.append('<option selected="true" disabled>Valitse lähtöasema:</option>');
+    dropdownlahto.prop('selectedIndex', 0);
+    const url = 'https://rata.digitraffic.fi/api/v1/metadata/stations';
+// Populate dropdown with list of provinces
+    $.getJSON(url, function (data) {
+        $.each(data, function (key, entry) {
+            dropdownlahto.append($('<option></option>').attr('value', entry.stationShortCode).text(entry.stationName));
+        })
+    });
+    // luodaan pääteasemien dynaaminen lista JSON:in avulla
+    let dropdownpaate = $('#paate');
+    dropdownpaate.empty();
+    dropdownpaate.append('<option selected="true" disabled>Valitse päätasema:</option>');
+    dropdownpaate.prop('selectedIndex', 0);
+    const url2 = 'https://rata.digitraffic.fi/api/v1/metadata/stations';
+    // Populate dropdown with list of provinces
+    $.getJSON(url2, function (data) {
+        $.each(data, function (key, entry) {
+            dropdownpaate.append($('<option></option>').attr('value', entry.stationShortCode).text(entry.stationName));
+        })
+    });
+    // ei voi valita kahta samaa asemaa
+    $(document).ready(function() {
+        $(".preferenceSelect").change(function() {
+            // Get the selected value
+            var selected = $("option:selected", $(this)).val();
+            // Get the ID of this element
+            var thisID = $(this).attr("id");
+            // Reset so all values are showing:
+            $(".preferenceSelect option").each(function() {
+                $(this).prop("disabled", false);
+            });
+            $(".preferenceSelect").each(function() {
+                if ($(this).attr("id") != thisID) {
+                    $("option[value='" + selected + "']", $(this)).attr("disabled", true);
+                }
+            });
+        });
+    });
+};
