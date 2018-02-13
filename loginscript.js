bebcfdd8e51f3c939fdcb72f4d56de892b3d0a6a
anvais <antti.j.valtonen@alumni.aalto.fi>
2018-02-13 18:08:27 +0200

    Antin muutokset.
    login.html tiedostoon luotu "Luo tili" nappi, joka kutsuu loginscript.js tiedoston uusiKayttaja() metodia
    
    loginscript.js tiedostoon luotu uusi metodi uusiKayttaja(), joka luo uuden käyttäjän. Uutta käyttäjää ei voi luoda, jos sen niminen jo löytyy, eikä sitä voi luoda pelkästään tunnuksella tai salasanalla. Uusi käyttäjä tallennetaan LocalStorage.kayttajat listan jatkoksi.

--- loginscript.js
+++ loginscript.js
@@ -1,16 +1,39 @@
 // Kun sivu avataan, latautuu testikäyttäjät localStorageen nimeltä kayttajat.
 // Jatkossa tässä lisätään myös uudet käyttäjät, kun softa julkaistaan
-window.onload = function tallenna() {
-    var niki = {"id":"niki", "pw":"salasana"};
-    var kake = {"id":"kake", "pw":"salis"};
-    var jake = {"id":"jake", "pw":"palis"};
-    localStorage.kayttajat = JSON.stringify(niki);
-    localStorage.kayttajat += JSON.stringify(kake);
-    localStorage.kayttajat += JSON.stringify(jake);
-}
+// window.onload = function tallenna() {
+//      var niki = {"id":"niki", "pw":"salasana"};
+//      var kake = {"id":"kake", "pw":"salis"};
+//      var jake = {"id":"jake", "pw":"palis"};
+//      localStorage.kayttajat = JSON.stringify(niki);
+//  };
+// YLLÄOLEVA NYT YLIMÄÄRÄISTÄ!
+
+
+// Tämä funktio luo uuden käyttäjätunnuksen, jos Luo tili-painiketta painetaan.
+// Lisätään uusi merkkijono LocalStorageen jo olemassaolevien jatkeeksi!
+    function uusiKayttaja(){
+        var kayttajat = localStorage.kayttajat;
+        var uusiNimi = document.getElementById("id").value;
+        var uusiSalasana = document.getElementById("pw").value;
+        if (kayttajat.indexOf(uusiNimi) === -1){
+            if (uusiSalasana === '') {
+                window.alert("Syötä myös salasana!")
+            } else if (uusiNimi === ''){
+                window.alert("Syötä myös käyttäjätunnus!")
+            } else {
+                var uusiKirjautuja = "{\"id\":\"" + uusiNimi + "\",\"pw\":\"" + uusiSalasana + "\"}";
+                localStorage.kayttajat += uusiKirjautuja;
+                window.alert("Uusi käyttäjätunnus on nyt luotu. Voit kirjautua järjestelmään syöttämilläsi tiedoilla!");
+                document.getElementById("id").value = '';
+                document.getElementById("pw").value = '';
+            }
+        } else {
+            window.alert("Käyttäjätunnus on jo olemassa! Yritä uudestaan.")
+        }
+    }
 // Tämä lukee käyttäjän syöttämät tunnukset ja vertaa niitä "kayttajat" tallenteeseen.
 // Jos löytyy vastaavuus, suoritetaan kirjautuminen ja siirry funktion kutsu.
-// Muuten pyydetään tunnuksia uudestaana
+// Muuten pyydetään tunnuksia uudestaan
     function lue(){
         var kayttajat = localStorage.kayttajat;
         var kirjautuja = "{\"id\":\"" + document.getElementById("id").value + "\",\"pw\":\"" + document.getElementById("pw").value + "\"}";
