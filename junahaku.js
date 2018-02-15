025a68f7e6fa657545b08b709ef0cb68bea5931e
nikisem <niki.semenkov@gmail.com>
2018-02-15 17:02:38 +0200

    Siistitty koodia. Lisätty/muokattu kommentteja. Testattu. Toimii!

--- junahaku.js
+++ junahaku.js
@@ -1,12 +1,15 @@
+//Esitellään globaaleja muuttujia
+
 req = new XMLHttpRequest();
 var asematUrl = 'https://rata.digitraffic.fi/api/v1/metadata/stations';
 var asemat;
-
 var lahtoasema;
 var paateasema;
 var paivamaara;
 
-
+/**
+ * Funktio avaa rest -yhteyden käyttäjän antamien tietojen perusteella ja luo kirjautuneelle suosikkinappulan.
+ */
 function hae(){
     document.getElementById("lista").innerHTML ='';
     lahtoasema = document.getElementById("lahto").value;
@@ -17,18 +20,17 @@ function hae(){
     pvm2= pvm2.toISOString();
 
     req.open('GET', 'https://rata.digitraffic.fi/api/v1/live-trains/station/' + lahtoasema + '/' + paateasema + '?startDate=' + pvm2 + '&limit=15', true);
-
     req.send(null);
     // Onnistuneen haun jälkeen tsekataan onko käyttäjä kirjautunut. Jos on, luodaan nappi, jolla käyttäjä voi tallentaa hakutietonsa LocalStorageen tulevaisuutta ajatellen.
     if ((window.location.href.indexOf("#") !== -1)){
         document.getElementById("faviNappi").innerHTML = "<button class=\"btn btn-block\" id=\"favnappi\" onclick=\"lisaaSuosikki()\">Lisää reitti suosikkeihin</button>";
-        // $('#favnappi').hide().delay(1000).fadeIn(2200);
     }
 }
 
-// sijoittaa suosikin hakuehtoihin, mutta ei toteuta hakua. Pitää painaa "Hae" erikseen.
+/**
+ * Sijoittaa suosikin hakuehtoihin ja toteuttaa haun.
+ */
 function avaaSuosikki(){
-    // var splitattava = document.getElementById("suosikki").innerText;
     var kayttajanNimi = window.location.href.substring((window.location.href.indexOf("#")+1), window.location.href.length);
     var splitattava= localStorage.getItem(kayttajanNimi)
     var splitattu = splitattava.split(" ");
@@ -42,12 +44,17 @@ function avaaSuosikki(){
     hae();
 }
 
-// kirjaa ulos käyttäjän, eli avaa uuden sivun ilman käyttäjätietoja html:ssä ja urlissa
+/**
+ * Kirjaa ulos käyttäjän.
+ */
 function kirjauduUlos() {
     window.alert("Olet nyt kirjautunut ulos. Tervetuloa uudestaan!");
     open(url="index.html","_self");
 }
-//tähän rakennetaan toiminnallisuus, joka lisää käyttäjän suosikin muistiin
+
+/**
+ * Tallentaa haetun reitin suosikiksi.
+ */
 function lisaaSuosikki(){
     window.alert("Tallensit reitin");
     lahtoasema = document.getElementById("lahto").value;
@@ -57,9 +64,9 @@ function lisaaSuosikki(){
     localStorage.setItem(kayttajanNimi,lahtoasema + " - " + paateasema);
 }
 
-//Laskee etäisyyden kahden pisteen väliltä. Parametreina kahden pisteen
-//latitude ja langitude. Käyttää apufunktiona muutaRadiaaniksi-funktiota.
-//Palauttaa etäisyyden kilometreinä.
+/**
+ * Laskee etäisyyden kahden pisteen väliltä. Parametreina kahden pisteen latitude ja longitude. Käyttää apufunktiona muutaRadiaaniksi-funktiota. Palauttaa etäisyyden kilometreinä.
+ */
 function laskeEtaisyys(lat1, lon1, lat2, lon2) {
     lat1 = muutaRadiaaniksi(lat1);
     lat2 = muutaRadiaaniksi(lat2);
@@ -72,12 +79,14 @@ function laskeEtaisyys(lat1, lon1, lat2, lon2) {
     return d;
 }
 
-//tämä funktio muuttaa asteet radiaaneiksi
+/**
+ * Tämä funktio muuttaa asteet radiaaneiksi.
+ */
 function muutaRadiaaniksi(deg) {
     return deg * Math.PI / 180;
 }
 
-// TÄSSÄ LUODAAN BACK TO TOP-NAPPI!
+//Luodaan "top" -nappi, joka palauttaa käyttäjän sivun alkuun.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -87,15 +96,16 @@ function scrollFunction() {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
-window.onload = function () {
-    //asettaa päivämäärän valinta -kenttään default-arvoksi tämän päivän
 
+window.onload = function () {
+
+    // Asettaa päivämäärän valinta -kenttään default-arvoksi tämän päivän
     var date = new Date();
     var day = date.getDate();
     var month = date.getMonth() + 1;
@@ -116,7 +126,7 @@ window.onload = function () {
         document.getElementById("piilotettava").style.display = "block";
     }
 
-    //printtaa kirjautuneen nimimerkin sivulle, luo kirjaudu ulos napin ja luo uuden localstoragen käyttäjälle, jos ei jo olemassa.
+    // Printtaa kirjautuneen nimimerkin sivulle, luo kirjaudu ulos napin ja luo uuden localstoragen käyttäjälle, jos ei jo olemassa.
     if (window.location.href.indexOf("#") !== -1){
         var kayttajanNimi = window.location.href.substring((window.location.href.indexOf("#")+1), window.location.href.length);
         console.log(kayttajanNimi);
@@ -129,6 +139,7 @@ window.onload = function () {
             document.getElementById("suosikkiReitti").innerHTML = "<button id=\"suosikki\" class='btn btn-block' onclick=\"avaaSuosikki()\">Hae suosikkireittiä: " + localStorage.getItem(kayttajanNimi)+"</button>";
         }
     }
+
     // TÄSSÄ PIILOTETAAN KIRJAUDU ULOS PAINIKE JOS EI KIRJAUTUNUT SISÄÄN, JOS KIRJAUTUNUT NIIN VAIN TÄMÄ NÄKYVILLÄ!
     if ((window.location.href.indexOf("#") !== -1)){
         document.getElementById("kirjauduUlosPiilotettava").style.display = "block";
@@ -136,7 +147,7 @@ window.onload = function () {
         document.getElementById("kirjauduUlosPiilotettava").style.display = "none";
     }
 
-    //haetaan asema-data, suodatetaan pois ne asemat, jotka eivät ole matkustaja-asemia
+    // Haetaan asema-data, suodatetaan pois ne asemat, jotka eivät ole matkustaja-asemia.
     $.getJSON(asematUrl, function (jsondata) {
         asemat = jsondata;
         var matkustajaAsemat= $.grep(asemat,function (mat) {
@@ -146,7 +157,7 @@ window.onload = function () {
     });
 
     estaSamaValintaDropdowneissa();
-    //haetaan käyttäjän geolokaatio ja tallennetaan omaLat- ja omaLon-muuttujiin
+    // Haetaan käyttäjän geolokaatio ja tallennetaan omaLat- ja omaLon-muuttujiin.
     var omaLat;
     var omaLon;
     navigator.geolocation.getCurrentPosition(
@@ -160,32 +171,15 @@ window.onload = function () {
         { enableHighAccuracy: true }
     );
 
-    //käydään läpi kaikki asemat ja etsitään lähin asema
-    //esitellään muuttujat, etäisyyden alkuarvo = "ääretön"
+    // Käydään läpi kaikki asemat ja etsitään lähin asema.
+    // Esitellään muuttujat, etäisyyden alkuarvo = "ääretön".
     var lahinAsema;
     var etaisyys = Number.MAX_VALUE;
 
-    //funktio käynnistyy napin painalluksella
+    // Funktio käynnistyy napin painalluksella.
     document.getElementById("haeAsema").onclick = function(){
-        //testitapauksia
-        //1: Kamppi
-        // omaLat = 60.166296;
-        // omaLon = 24.932770;
-        //2: Seinäjoen maaseutu
-        // omaLat = 62.553988;
-        // omaLon = 23.004272;
-        // //3: Lappi
-        // omaLat = 67.456916;
-        // omaLon = 27.656982;
-        // //4: Pitäjänmäki
-        // omaLat = 60.228396;
-        // omaLon = 24.847777;
 
-        //varmistetaan, millä tiedoilla haku tehdään
-        console.log('Oma leveyspiiri: ' + omaLat);
-        console.log('Oma pituuspiiri: ' + omaLon);
-
-        //käydään läpi asemat ja haetaan pienintä etäisyyttä (linnuntietä)
+        // Käydään läpi asemat ja haetaan pienintä etäisyyttä (linnuntietä).
         for(var i = 0; i < asemat.length; i++){
             var asemanLat = asemat[i].latitude;
             var asemanLon = asemat[i].longitude;
@@ -196,36 +190,30 @@ window.onload = function () {
             }
         }
 
-        console.log(etaisyys);
-        console.log(lahinAsema.stationName);
-
-        //sijoitetaan alasvetovalikkoon lähin asema
+        // Sijoitetaan alasvetovalikkoon lähin asema.
         $('#lahto').val(lahinAsema.stationShortCode);
     }
-
-
 
     req.onreadystatechange = function () {
         if(req.readyState === 4){
             if (req.status === 200){
-                console.dir(asemat);
                 var data = JSON.parse(req.responseText);
-                console.dir(data);
 
-                //varoitetaan, jos yhteyksiä ei löydy, ei turhaan tulosteta tyhjää lähtöasemaa
+                // Varoitetaan, jos yhteyksiä ei löydy, ei turhaan tulosteta tyhjää lähtöasemaa.
                 if(data.code === "TRAIN_NOT_FOUND"){
                     window.alert("Yhteydelle ei löydy suoria junia! Valitse toinen yhteys.");
                 }
 
-                //luodaan taulukko, johon junat lisätään
+                // Luodaan taulukko, johon junat lisätään.
                 $('<table></table>', {
                     id: 'taulukko',
                     class: 'table table-responsive table-hover'
                 }).appendTo('#lista');
-                //lisätään headerit taulukolle
+
+                // Lisätään headerit taulukolle.
                 $('#taulukko').append('<thead><tr><th></th><th>Juna</th><th>Lähtöaika</th><th>Saapumisaika</th>');
 
-                //haetaan dokumentin kohta, johon taulukon tiedot lisätään (käytetään for-luupissa alla)
+                // Haetaan dokumentin kohta, johon taulukon tiedot lisätään (käytetään for-luupissa alla).
                 var taulukonTaytto = document.getElementById('taulukko');
 
                 for(var i = 0; i < data.length; i++){
@@ -234,18 +222,18 @@ window.onload = function () {
                     var paateasemanIndeksi = palautaJunanTiedot(paateasema, juna.timeTableRows);
                     var lahtoaika = new Date(juna.timeTableRows[lahtoasemanIndeksi].scheduledTime);
                     var saapumisaika = new Date(juna.timeTableRows[paateasemanIndeksi].scheduledTime);
-                    //jos lähijuna, junan nimi = lähijunan tunnus, muuten junan tyyppi + junan numero
+                    // Jos lähijuna, junan nimi = lähijunan tunnus, muuten junan tyyppi + junan numero.
                     var junanNimi = (juna.trainCategory==="Commuter")? (juna.commuterLineID+"-juna") : (juna.trainType+juna.trainNumber);
                     var optiot = {hour: '2-digit', minute: '2-digit', hour12: false}; //ajan näyttämisen asetukset
 
-                    //lisätään taulukkoon rivi, jossa on junan tiedot
+                    // Lisätään taulukkoon rivi, jossa on junan tiedot.
                     taulukonTaytto.innerHTML += '<tr class="clickable" data-toggle="collapse" id=' + juna.trainNumber
                         + ' data-target=.' + juna.trainNumber + '><td><i class="glyphicon glyphicon-plus"></i></td><td>'+junanNimi+'</td>' +
                         '<td>'+ lahtoaika.toLocaleDateString("fi", optiot) + '</td>' +
                         '<td>' + saapumisaika.toLocaleDateString("fi", optiot) + '</td></tr>';
 
-                    //haetaan niiden asemien tiedot, joissa juna pysähtyy
-                    //loopin alku vaati säätöä, että se toimii silloin, kun lähtöpaikka on junan eka asema
+                    // Haetaan niiden asemien tiedot, joissa juna pysähtyy.
+                    // Loopin alku vaati säätöä, että se toimii silloin, kun lähtöpaikka on junan eka asema.
                     var loopinAlku;
                     var aika;
                     var asemanNimi;
@@ -276,7 +264,9 @@ window.onload = function () {
         }
     };
 
-    //funktio joka palauttaa aseman koko nimen shortCoden perusteella
+    /**
+     * Palauttaa aseman koko nimen shortCoden perusteella.
+     */
     function palautaAsemanTiedot(syote) {
         var a = $.grep(asemat, function (e) {
             return (e.stationShortCode === syote);
@@ -284,53 +274,51 @@ window.onload = function () {
         return a[0];
     }
 
-    //funktio joka palauttaa haetun aseman indeksin junan tiedoista
+    /**
+     * Palauttaa haetun aseman indeksin junan tiedoista.
+     */
     function palautaJunanTiedot(asemakoodi, data) {
         return data.findIndex(x => x.stationShortCode === asemakoodi);
     }
 
-    // luodaan valittujen asemien väliset yhteydet listaksi
+    // Luodaan valittujen asemien väliset yhteydet listaksi.
     document.getElementById("nappi").onclick = hae;
 
-
-
-    //funktio, joka luo dropdownmenut asema-json-datan perusteella
+    /**
+     * Luodaan dropdownmenut asema-json-datan perusteella.
+     */
     function luoDropdownMenut(data){
         // luodaan lähtöasemien dynaaminen lista JSON:in avulla
-        let dropdownlahto = $('#lahto');
+        var dropdownlahto = $('#lahto');
         dropdownlahto.empty();
         dropdownlahto.append('<option selected="true" disabled>Valitse lähtöasema:</option>');
         dropdownlahto.prop('selectedIndex', 0);
         luoLahtoasemat(data);
 
-// Populate dropdown with list of provinces
         function luoLahtoasemat(data) {
             $.each(data, function (key, entry) {
                 dropdownlahto.append($('<option></option>').attr('value', entry.stationShortCode).text(entry.stationName));
             })
         };
-        // luodaan pääteasemien dynaaminen lista JSON:in avulla
-        let dropdownpaate = $('#paate');
+        // Luodaan pääteasemien dynaaminen lista JSON:in avulla.
+        var dropdownpaate = $('#paate');
         dropdownpaate.empty();
         dropdownpaate.append('<option selected="true" disabled>Valitse pääteasema:</option>');
         dropdownpaate.prop('selectedIndex', 0);
         luoPaateasemat(data);
 
-        // Populate dropdown with list of provinces
         function luoPaateasemat(data) {
             $.each(data, function (key, entry) {
                 dropdownpaate.append($('<option></option>').attr('value', entry.stationShortCode).text(entry.stationName));
             })
         };
     };
-    // ei voi valita kahta samaa asemaa, pitää tehdä erillinen funktio tästä ja siirtää myös kutsu hakufunktion ekaksi riviksi
+
+    // Ei voi valita kahta samaa asemaa.
     function estaSamaValintaDropdowneissa () {
         $(".preferenceSelect").change(function() {
-            // Get the selected value
             var selected = $("option:selected", $(this)).val();
-            // Get the ID of this element
             var thisID = $(this).attr("id");
-            // Reset so all values are showing:
             $(".preferenceSelect option").each(function() {
                 $(this).prop("disabled", false);
             });
@@ -345,9 +333,9 @@ window.onload = function () {
 
 // LOGIN scriptiä
 
-// Tämä funktio luo uuden käyttäjätunnuksen, jos Luo tili-painiketta painetaan.
-// Lisätään uusi merkkijono LocalStorageen jo olemassaolevien jatkeeksi!
-// ALERT-VIESTEISSÄ VIELÄ HIOMISEN VARAA!
+/**
+ * Luodaan uusi käyttäjätunnus, jos Luo tili-painiketta painetaan.
+ */
 function luoJaLisaaUusiKayttaja(){
     var kayttajat = localStorage.kayttajat;
     var uusiNimi = document.getElementById("id").value;
@@ -368,7 +356,7 @@ function luoJaLisaaUusiKayttaja(){
             document.getElementById("pw").value = '';
         }
     } else {
-        //otetaan kiinni tyhjä käyttäjätunnus
+        // Otetaan kiinni tyhjä käyttäjätunnus.
         if(kayttajat.indexOf(uusiNimi) === 0 && uusiSalasana === ''){
             window.alert("Syötä käyttäjätunnus ja salasana!");
         } else if (kayttajat.indexOf(uusiNimi) === 0 && uusiSalasana !== ''){
@@ -393,9 +381,10 @@ function luoJaLisaaUusiKayttaja(){
     }
 }
 
-// Tämä lukee käyttäjän syöttämät tunnukset ja vertaa niitä "kayttajat" tallenteeseen.
-// Jos löytyy vastaavuus, suoritetaan kirjautuminen ja siirry funktion kutsu.
-// Muuten pyydetään tunnuksia uudestaan
+/**
+ * Tämä lukee käyttäjän syöttämät tunnukset ja vertaa niitä "kayttajat" tallenteeseen.
+ * Jos löytyy vastaavuus, suoritetaan kirjautuminen ja siirry funktion kutsu. Muuten pyydetään tunnuksia uudestaan.
+ */
 function kirjautuuSisaanJosTunnuksetOikein() {
     var kayttajat = localStorage.kayttajat;
     var kirjautuja = "{\"id\":\"" + document.getElementById("id").value + "\",\"pw\":\"" + document.getElementById("pw").value + "\"}";
@@ -413,7 +402,10 @@ function kirjautuuSisaanJosTunnuksetOikein() {
         siirryKirjautuneenaAikatauluSivulle();
     }
 }
-// Tässä avataan alkuperäinen junien aikataulusivu, jonka urlin perään on asetettu juuri onnistuneesti sisäänkirjautuneen käyttäjän käyttäjätunnus
+
+/**
+ * Tässä avataan alkuperäinen junien aikataulusivu, jonka urlin perään on asetettu juuri onnistuneesti sisäänkirjautuneen käyttäjän käyttäjätunnus.
+ */
 function siirryKirjautuneenaAikatauluSivulle() {
     window.location.reload();
     open(url="index.html" + "#" + document.getElementById("id").value,"_self");
